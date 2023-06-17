813d6a71df24b84023f281457d278f90d7cc3d99
hongyunsu <dbstn9@hanmail.net>
2023-06-17 18:03:00 +0900

    [🔨]Refactor: Sidebar 리펙토링

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import styled from "styled-components";
-import {
-	AiFillHome,
-	AiFillCalendar,
-	AiFillMessage,
-	AiFillSetting,
-} from "react-icons/ai";
-import { FaUser } from "react-icons/fa";
 import { BiPencil } from "react-icons/bi";
+
+import Sidebar from "./components/Sidebar";
 
 const Container = styled.div`
 	display: flex;
@@ -24,24 +19,6 @@ const InnerContainer = styled.div`
 	background-color: white;
 	border-radius: 40px;
 	margin: 20px;
-`;
-
-const SideBar = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-around;
-	background-color: #ffdada;
-	border-radius: 30px;
-	margin: 30px;
-	svg {
-		margin-left: 10px;
-		margin-right: 10px;
-		font-size: 45px;
-		color: white;
-		&:hover {
-			color: #ff9292;
-		}
-	}
 `;
 
 const Main = styled.div`
@@ -112,13 +89,7 @@ function App() {
 	return (
 		<Container>
 			<InnerContainer>
-				<SideBar>
-					<AiFillHome />
-					<AiFillCalendar />
-					<AiFillMessage />
-					<FaUser />
-					<AiFillSetting />
-				</SideBar>
+				<Sidebar />
 				<Main>
 					<MainTitle>All-in-one schedule</MainTitle>
 					<MainBody>
