f03895bc69daa1784f665ee5313ea35cbec638ef
hongyunsu <dbstn9@hanmail.net>
2023-06-19 21:25:47 +0900

    [:wrench:Add]: todolist 아이템 추가

---
+++ client/src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TodoItem({ todo }) {
+	const { id, text, checked } = todo;
+
+	return (
+		<div>
+			<div></div>
+		</div>
+	);
+}
+
+export default TodoItem;
