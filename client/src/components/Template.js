576637cb1fa56afba39ea53deb900f71b161e062
hongyunsu <dbstn9@hanmail.net>
2023-06-25 19:03:02 +0900

    [:art:Add]: Template styled 컴포넌트  수정

--- client/src/components/Template.js
+++ client/src/components/Template.js
@@ -1,11 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	max-height: 100vh;
+`;
 
 function Template({ children }) {
 	return (
-		<div className="Template">
+		<Container>
 			<div className="title">오늘의 할일 (todo길이)</div>
 			<div>{children}</div>
-		</div>
+		</Container>
 	);
 }
 
